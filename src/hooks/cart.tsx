f10f56281c5e3ca15eebb9372072f8e32702a22b
jerp86 <jerp4@hotmail.com>
2020-04-27 21:03:39 -0300

    Correção no desafio sem ajustes nos testes

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -29,27 +29,21 @@ const CartContext = createContext<CartContext | null>(null);
 const CartProvider: React.FC = ({ children }) => {
   const [products, setProducts] = useState<Product[]>([]);
 
-  // function sortable(lista: Product[]): Product[] {
-  //   return lista.sort((a, b) => {
-  //     if (a.title > b.title) {
-  //       return 1;
-  //     }
-  //     if (a.title < b.title) {
-  //       return -1;
-  //     }
-  //     return 0;
-  //   });
-  // }
-
   useEffect(() => {
     async function loadProducts(): Promise<void> {
       // TODO LOAD ITEMS FROM ASYNC STORAGE
       const response = await AsyncStorage.getItem(STORAGE_KEY);
 
-      // if (response) {
-      // setProducts(sortable([...JSON.parse(response)]));
-      setProducts([...JSON.parse(response)]);
-      // }
+      if (response) {
+        const storageProducts = [...JSON.parse(response)].sort((a, b) => {
+          if (a.title > b.title) {
+            return 1;
+          }
+          return a.title < b.title ? -1 : 0;
+        });
+
+        setProducts(storageProducts);
+      }
     }
 
     loadProducts();
@@ -59,23 +53,28 @@ const CartProvider: React.FC = ({ children }) => {
     async product => {
       const productExists = products.find(prod => prod.id === product.id);
       const quantity = productExists ? productExists.quantity + 1 : 1;
-
       const newProductValue = { ...product, quantity };
 
       if (productExists) {
-        // const newProducts = sortable(
-        //   products.map(prod =>
-        //     prod.id === product.id ? newProductValue : prod,
-        //   ),
-        // );
-        const newProducts = products.map(prod =>
-          prod.id === product.id ? newProductValue : prod,
+        setProducts(
+          products
+            .map(p => (p.id === product.id ? newProductValue : p))
+            .sort((a, b) => {
+              if (a.title > b.title) {
+                return 1;
+              }
+              return a.title < b.title ? -1 : 0;
+            }),
         );
-        setProducts(newProducts);
       } else {
-        // const newProducts = sortable([...products, newProductValue]);
-        const newProducts = [...products, newProductValue];
-        setProducts(newProducts);
+        setProducts(
+          [...products, newProductValue].sort((a, b) => {
+            if (a.title > b.title) {
+              return 1;
+            }
+            return a.title < b.title ? -1 : 0;
+          }),
+        );
       }
       await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(products));
     },
@@ -85,24 +84,20 @@ const CartProvider: React.FC = ({ children }) => {
   const increment = useCallback(
     async id => {
       // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-      // const productIndex = products.findIndex(product => product.id === id);
-
-      // if (productIndex >= 0) {
-      //   const filterProducts = products.filter(product => product.id !== id);
-
-      //   const newProduct = products[productIndex];
-      //   newProduct.quantity += 1;
-
-      //   const newArray = sortable([...filterProducts, newProduct]);
-
-      //   setProducts(newArray);
-
-      //   await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(newArray));
-      // }
-      const newProducts = products.map(prod =>
-        prod.id === id ? { ...prod, quantity: prod.quantity + 1 } : prod,
+      setProducts(
+        products
+          .map(product =>
+            product.id === id
+              ? { ...product, quantity: product.quantity + 1 }
+              : product,
+          )
+          .sort((a, b) => {
+            if (a.title > b.title) {
+              return 1;
+            }
+            return a.title < b.title ? -1 : 0;
+          }),
       );
-      setProducts(newProducts);
 
       await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(products));
     },
@@ -112,56 +107,26 @@ const CartProvider: React.FC = ({ children }) => {
   const decrement = useCallback(
     async id => {
       // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
-      const productIndex = products.findIndex(product => product.id === id);
+      setProducts(
+        products
+          .map(product =>
+            product.id === id
+              ? { ...product, quantity: product.quantity - 1 }
+              : product,
+          )
+          .filter(product => product.quantity > 0)
+          .sort((a, b) => {
+            if (a.title > b.title) {
+              return 1;
+            }
+            return a.title < b.title ? -1 : 0;
+          }),
+      );
 
-      // if (productIndex >= 0) {
-
-      // const newProduct = products[productIndex];
-
-      if (products[productIndex].quantity <= 1) {
-        const filterProducts = products.filter(product => product.id !== id);
-        setProducts(filterProducts);
-
-        // await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(filterProducts));
-      } else {
-        // newProduct.quantity -= 1;
-
-        // const newValues = sortable([...filterProducts, newProduct]);
-
-        // setProducts(newValues);
-
-        // await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(newValues));
-        const newProducts = products.map(prod =>
-          prod.id === id ? { ...prod, quantity: prod.quantity - 1 } : prod,
-        );
-        setProducts(newProducts);
-      }
       await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(products));
     },
     [products],
   );
-
-  // const addToCart = useCallback(
-  //   async product => {
-  //     // TODO ADD A NEW ITEM TO THE CART
-  //     const productIndex = products.findIndex(p => p.id === product.id);
-
-  //     if (productIndex < 0) {
-  //       const newProduct = { ...product, quantity: 1 };
-  //       setProducts(oldSate => sortable([...oldSate, newProduct]));
-
-  //       await AsyncStorage.setItem(
-  //         STORAGE_KEY,
-  //         JSON.stringify(sortable([...products, newProduct])),
-  //       );
-
-  //       return;
-  //     }
-
-  //     increment(product.id);
-  //   },
-  //   [increment, products],
-  // );
 
   const value = React.useMemo(
     () => ({ addToCart, increment, decrement, products }),
